aad8415ac877f9a2d58a16db388a41bff416b00f
Matt Hoofman <mhoofman@gmail.com>
2010-12-29 12:55:16 -0500

    added files to gitignore and created sample config

---
+++ sample.config.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_FLAVOR', 'mysql'); // mysql or sqlite
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '_urlshortener'); // for sqlite: path to db file
+define('SHORT_URL', 'http://www.example.com/'); // include the trailing slash!
+define('DEFAULT_URL', 'http://www.example.com'); // omit the trailing slash!
+?>
